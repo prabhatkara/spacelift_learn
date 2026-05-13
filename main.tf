abdb186d97b7d1bd552eb86fe559af7c0a6e3736
K PRABHAT KUMAR <45866083+prabhatkara@users.noreply.github.com>
2026-05-13 11:16:44 +0000

    updated variable position

--- main.tf
+++ main.tf
@@ -6,7 +6,10 @@ terraform {
     }
   }
 }
-
+variable "subnet_id" {
+  type        = string
+  description = "ID of the subnet from networking stack"
+}
 provider "aws" {
   region = "us-east-1"
 }
@@ -30,10 +33,7 @@ resource "aws_subnet" "main" {
 }
 
 
-variable "subnet_id" {
-  type        = string
-  description = "ID of the subnet from networking stack"
-}
+
 data "aws_subnet" "selected" {
   id = var.subnet_id
 }
