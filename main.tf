dc9e8c793fd15375692eccf0f3fcfe7e08f4b051
K PRABHAT KUMAR <45866083+prabhatkara@users.noreply.github.com>
2026-05-13 10:53:05 +0000

    auto deploy turned on

--- main.tf
+++ main.tf
@@ -27,3 +27,11 @@ resource "aws_s3_bucket" "orbit_storage" {
     project   = "Orbit-labs"
   }
 }
+
+tags = {
+  name        = "Orbit Labs Storage"
+  managedBy   = "Spacelift"
+  mission     = "First Launch"
+  project     = "Orbit-labs"
+  environment = "demo"
+}
