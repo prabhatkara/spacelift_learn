d63f27efadf83bfc0b58f25927a8f8336ee99ac7
K PRABHAT KUMAR <45866083+prabhatkara@users.noreply.github.com>
2026-05-13 11:22:04 +0000

    fetch subnet

--- main.tf
+++ main.tf
@@ -32,26 +32,3 @@ output "subnet_id" {
   value       = aws_subnet.main.id
   description = "ID of the main subnet"
 }
-variable "subnet_id" {
-  type        = string
-  description = "ID of the subnet from networking stack"
-}
-
-data "aws_subnet" "selected" {
-  id = var.subnet_id
-}
-
-data "aws_vpc" "selected" {
-  id = data.aws_subnet.selected.vpc_id
-}
-
-resource "aws_security_group" "app" {
-  name        = "orbit-labs-app-sg"
-  description = "Security group for Orbit Labs app"
-  vpc_id      = data.aws_vpc.selected.id
-
-  tags = {
-    name    = "Orbit Labs App SG"
-    project = "Orbit-labs"
-  }
-}
